ecd6c1b5c865dc5d547dbe9d49abad48362212c3
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-08-25 21:46:24 -0500

    Adding v-for and v-if exercise

---
+++ VueJS_Fundamentals/01.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        title: 'Hello world with Vue',
+        fruits: [
+            { name: 'Pear', quantity: 10 },
+            { name: 'Apple', quantity: 0 },
+            { name: 'Banana', quantity: 11 }
+        ]
+    }
+})
