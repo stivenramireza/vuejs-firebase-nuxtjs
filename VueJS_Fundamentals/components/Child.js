d74493d529930d03af179d4c8543a54d27c4c99e
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-09-07 12:04:38 -0500

    Adding Events Communication in Components

--- VueJS_Fundamentals/components/Child.js
+++ VueJS_Fundamentals/components/Child.js
@@ -2,8 +2,36 @@ Vue.component('Child', {
     template: //html
     `
     <div class="p-5 bg-success text-white">
-        <h3>Child Component: {{ childNumber }}</h3>
+        <h3>Child Component: {{ number }} - {{ childName }}</h3>
+        <button @click="number--" class="btn btn-primary">-</button>
     </div>
     `,
-    props: ['childNumber']
+    props: {
+        childNumber: {
+            type: Number,
+            required: true,
+            default: 0,
+            validator: value => {
+                return value >= 0
+            }
+        }
+    },
+    data() {
+        return {
+            childName: 'Stiven'
+        }
+    },
+    computed: {
+        number: {
+            getNumber() {
+                return this.childNumber
+            },
+            setNumber(val) {
+                this.$emit('update:number', val)
+            }
+        }
+    },
+    mounted() {
+        this.$emit('emitedName', this.childName)
+    }
 })
--- VueJS_Fundamentals/components/Parent.js
+++ VueJS_Fundamentals/components/Parent.js
@@ -2,14 +2,15 @@ Vue.component('Parent', {
     template: //html
     `
     <div class="p-5 bg-dark text-white">
-        <h3>Parent Component: {{ parentNumber }}</h3>
+        <h3 @emitedNumber="parentNumber = $event">Parent Component: {{ parentNumber }} - {{ parentName }}</h3>
         <button @click="parentNumber++" class="btn btn-danger">+</button>
-        <Child :childNumber="parentNumber"></Child>
+        <Child :childNumber="parentNumber" @emitedName="parentName = $event"></Child>
     </div>
     `,
     data() {
         return {
-            parentNumber: 0
+            parentNumber: 0,
+            parentName: '',
         }
     },
 })
