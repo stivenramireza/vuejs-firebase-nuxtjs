e70e543d9de666c09942356eb7ae3076904bcab0
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-11-25 16:38:07 -0500

    Adding new credentials

--- firebase-auth/src/main.js
+++ firebase-auth/src/main.js
@@ -13,12 +13,13 @@ require("firebase/auth");
 // require("firebase/functions");
 
 var config = {
-  apiKey: "AIzaSyDARb2DmuqWK0NWNFBVsRuVMu8CTOyXXYs",
-  authDomain: "crud-udemy.firebaseapp.com",
-  databaseURL: "https://crud-udemy.firebaseio.com",
-  projectId: "crud-udemy",
-  storageBucket: "crud-udemy.appspot.com",
-  messagingSenderId: "938509473843"
+  apiKey: "AIzaSyAseMe72xm1BI7ICtpCfkwpfy9DOmGlvcs",
+  authDomain: "fir-crud-6b5ee.firebaseapp.com",
+  databaseURL: "https://fir-crud-6b5ee.firebaseio.com",
+  projectId: "fir-crud-6b5ee",
+  storageBucket: "fir-crud-6b5ee.appspot.com",
+  messagingSenderId: "1065038021",
+  appId: "1:1065038021:web:75cfd7b38bef850b3e00ac"
 };
 firebase.initializeApp(config);
 
