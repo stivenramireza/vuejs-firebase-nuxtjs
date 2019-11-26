cd4429c85c3e8ad7736d642956d435d3e5c917d0
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-11-26 14:52:46 -0500

    Adding babel.config.js

---
+++ firebase-auth/src/views/Add.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Add</h1>
+        <form @submit.prevent="addTask(name)" class="form-inline">
+            <div class="input-group mb-2 mr-sm-2">
+                <div class="input-group-prepend">
+                <div class="input-group-text">Name</div>
+                </div>
+                <input type="text" class="form-control" v-model="name">
+            </div>
+            <button type="submit" class="btn btn-primary mb-2">Add</button>
+        </form>
+    </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+export default {
+    name: 'Add',
+    data(){
+        return{
+            name: ''
+        }
+    },
+    methods: {
+        ...mapActions(['addTask'])
+    }
+}
+</script>
---
+++ firebase-auth/src/views/Update.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1>Update</h1>
+        {{ id }} - {{ task }}
+        <form @submit.prevent="updateTask(task)" class="form-inline">
+            <div class="input-group mb-2 mr-sm-2">
+                <div class="input-group-prepend">
+                <div class="input-group-text">Name</div>
+                </div>
+                <input type="text" class="form-control" v-model="task.name">
+            </div>
+            <button type="submit" class="btn btn-primary mb-2">Update</button>
+        </form>
+    </div>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex'
+export default {
+    name: 'Update',
+    data(){
+        return{
+            id: this.$route.params.id
+        }
+    },
+    methods: {
+        ...mapActions(['getTask', 'updateTask'])
+    },
+    created(){
+        this.getTask(this.id)
+    },
+    computed: {
+        ...mapState(['task'])
+    }
+}
+</script>
