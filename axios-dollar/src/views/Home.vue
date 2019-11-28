a87b7cc53f22a117537c4482c8a2aa93a0940611
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-11-28 16:45:54 -0500

    Adding Axios

--- axios-dollar/src/views/Home.vue
+++ axios-dollar/src/views/Home.vue
@@ -1,18 +1,65 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+  <v-layout :wrap="true">
+      <v-flex xs12>
+
+        <v-card>
+          <v-date-picker 
+            v-model="date" 
+            :full-width="true"
+            locale="es-cl"
+            :min="minimum"
+            :max="maximum"
+            @change="getDollar(date)"
+          >
+          </v-date-picker>
+        </v-card>
+
+        <v-card color="error" dark>
+          <v-card-text class="display-1 text-center">
+            {{ dollar }}
+          </v-card-text>
+        </v-card>
+
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from 'axios'
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      date: new Date().toISOString().substr(0, 10),
+      minimum: '1984',
+      maximum: new Date().toISOString().substr(0, 10),
+      dollar : null
+    }
+  },
+  methods: {
+    async getDollar(day){
+      let arrayDate = day.split(['-'])
+      let ddmmyy = arrayDate.reverse().join('-')
+      try {
+        let data = await axios.get(`https://mindicador.cl/api/dolar/${ddmmyy}`)
+
+        if(data.data.serie.length > 0){
+          this.dollar = await data.data.serie[0].valor
+        }else{
+          this.dollar = 'No results'
+        }
+      } catch (error) {
+        console.log(error)
+      } finally {
+
+      }
+     
+    }
+  },
+  created() { 
+    this.getDollar(this.date)
   }
 }
 </script>
